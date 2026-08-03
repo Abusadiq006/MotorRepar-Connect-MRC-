3f3fa489f32e4e1c16fb7e1f8460016906d1aa15
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-08-03 09:17:17 -0700

    apps to use

--- client/src/constant/api.js
+++ client/src/constant/api.js
@@ -5,3 +5,10 @@ import helmet from "helmet"
 import morgan from "morgan"
 
 const app = express()
+
+app.use(helmet());
+app.use(compression());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
